227d99e6d0a48b378ea51ec0a9af8c15175a3b31
Salvatore Barone <salvator.barone@gmail.com>
2021-07-02 19:35:22 +0200

    feature: implements the catalog generation procedure

--- src/ALSWorker.py
+++ src/ALSWorker.py
@@ -1,14 +1,15 @@
-import sqlite3
+
 from pyosys import libyosys as ys
 from .ALSEpsMaxEvaluator import *
 from .ALSErsEvaluator import *
+from .ALSCatalog import *
 
 class ALSWorker:
   def __init__(self, module, rewrite, luts_t, catalog, metric, weights, attempts, iterations, nvectors, debug):
     self.__module = module
     self.__rewrite = rewrite
     self.__luts_t = luts_t
-    self.__catalog_db = catalog
+    self.__catalog = ALSCatalog(catalog)
     self.__metric = metric
     self.__iterations = iterations
     self.__weights = weights
@@ -16,7 +17,7 @@ class ALSWorker:
     self.__nvectors = nvectors
     self.__debug = debug
     self.__synthesized_luts = None
-    self.__db_conn = None
+    
   """
   @brief Implements the whole Catalog-based AIG rewriting (C-AIGRW) approximate technique workflow
 
@@ -36,10 +37,6 @@ class ALSWorker:
   hardware requirements will be lower than the original circuit.
   """
   def run(self):
-    try:
-      self.__db_conn = sqlite3.connect(self.__catalog_db)
-      print(sqlite3.version)
-
       #if self.__rewrite:
       #  self.__rewrite_run()
       #  return
@@ -61,12 +58,7 @@ class ALSWorker:
 
       ys.log_header(self.__module.design, "Rolling-back all rewrites.\n")
       ys.log_pop()
-    except sqlite3.Error as e:
-      print(e)
-    finally:
-      if self.__db_conn:
-        self.__db_conn.close()
-
+    
   def print_archive(self):
     # TODO: to be implemented
     pass
@@ -86,18 +78,50 @@ class ALSWorker:
     # TODO: to be implemented
     pass
 
-  def __catalog_generation(self):
-    luts_set = {}
-    for cell in self.__module.cells:
-      print(cell.getParam("\\LUT"))
-      luts_set.append(cell)
-    pass
+  """
+  @brief Catalog generation procedure
 
-  def __optimize(self):
+  @details
+  Starting from the exact specification of each unique LUT in the considered circuit, we progressively increase the
+  Hamming distance between the function being implemented by original LUT (cut) and the approximate one, while 
+  performing Exact Synthesis. 
+  
+  The procedure stops when, due to the approximation itself, the synthesis becomes trivial, i.e. it results in a catalog
+  entry of size zero.
+  """
+  def __catalog_generation(self):
+    luts_set = set()
+    for module in self.__module.design.selected_whole_modules_warn():
+      for cell in module.selected_cells():
+        if ys.IdString("\LUT") in cell.parameters:     
+          print(cell.parameters[ys.IdString("\LUT")].as_string())
+          luts_set.add(cell.parameters[ys.IdString("\LUT")])
+
+    # Sinthesizing the baseline lut and, then, approximate ones
+    # This for loop can be partitioned among multiple threads
+    for lut in luts_set:
+      hamming_distance = 0;
+      result = self.__get_synthesized_lut(lut, 0)
+      while True: #TODO replace True with result.aig.gates > 0
+        hamming_distance += 1
+        result = self.__get_synthesized_lut(lut, hamming_distance)
+
+  def __get_synthesized_lut(self, lut, distance):
+    result = self.__catalog.get_lut_at_dist(lut, distance)
+    if result is None:
+      ys.log("Cache miss for lut " + lut.as_string() + " at distance " + str(distance) + "\n")
+      ys.log("Performing exact synthesis...")
+      result = self.__synthesize_lut(lut, distance)
+      ys.log(" Done\n")
+      self.__catalog.add_lut(lut, result)
+    else:
+      ys.log("Cache miss for lut " + lut.as_string() + " at distance " + str(distance) + "\n")
+    return result
+
+  def __synthesize_lut(self, lut, distance):
     # TODO: to be implemented
     pass
 
-  @staticmethod
-  def __synthesize_lut(lut, distance, es_max_attempts, debug, db_conn):
+  def __optimize(self):
     # TODO: to be implemented
     pass
