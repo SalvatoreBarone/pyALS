1ab9cefead8f972cf7fdd7bccd777e9f03d5e3fd
Salvatore <salvator.barone@gmail.com>
2021-12-20 13:54:59 +0100

    feature: implements AWCE error metric and configurable error-evaluation

--- src/Error.py
+++ src/Error.py
@@ -26,55 +26,87 @@ class ErrorConfig:
         ERS = 1
         AWCE = 2
 
-    def __init__(self, metric, threshold, vectors):
+    def __init__(self, metric, threshold, vectors, weights = None):
         error_metrics = {"ers": ErrorConfig.Metric.ERS, "awce": ErrorConfig.Metric.AWCE}
         if metric not in ["ers", "awce"]:
             raise ValueError(f"{metric}: error-metric not recognized")
         else:
-            self.technique = error_metrics[metric]
+            self.metric = error_metrics[metric]
         self.threshold = threshold
         self.n_vectors = vectors
+        self.weights = weights
 
-
-class ERS(AMOSA.Problem):
+class ErrorEvaluator:
     def __init__(self, graph, catalog, n_vectors, threshold):
         self.graph = graph
-        graphs = [copy.deepcopy(graph)] * cpu_count()
-        n_vars = graph.get_num_cells()
+        self.graphs = [copy.deepcopy(graph)] * cpu_count()
+        self.n_vars = graph.get_num_cells()
         self.catalog = catalog
         self.n_vectors = n_vectors
         self.threshold = threshold
-        samples = []
+        self.samples = []
         PI = graph.get_pi()
         for _ in range(self.n_vectors):
             input = [{"name": i["name"], "value": bool(random.getrandbits(1))} for i in PI]
-            samples.append({"input": input, "output": graph.evaluate(input)})
-        self.__args = [[g, s, [0] * n_vars] for g, s in zip(graphs, list_partitioning(samples, cpu_count()))]
+            self.samples.append({"input": input, "output": graph.evaluate(input)})
         cells = [{"name": c["name"], "spec": c["spec"]} for c in graph.get_cells()]
-        upper_bound = [len(e) - 1 for c in cells for e in catalog if e[0]["spec"] == c["spec"]]
-        AMOSA.Problem.__init__(self, n_vars, [AMOSA.Type.INTEGER] * n_vars, [0] * n_vars, upper_bound, 2, 1)
-        configuration = self._matter_configuration([0] * n_vars)
-        and_gates = sum([c["gates"] for c in configuration])
-        print(f"# vars: {n_vars}, ub:{upper_bound}")
-        print(f"Baseline requirements: {and_gates} AIG-nodes")
+        self.upper_bound = [len(e) - 1 for c in cells for e in catalog if e[0]["spec"] == c["spec"]]
+        configuration = self._matter_configuration([0] * self.n_vars)
+        self.baseline_and_gates = self._get_gates(configuration)
+        print(f"# vars: {self.n_vars}, ub:{self.upper_bound}")
+        print(f"Baseline requirements: {self.baseline_and_gates} AIG-nodes")
 
     def _matter_configuration(self, x):
         return [{"name": l["name"], "dist": c, "spec": e[0]["spec"], "axspec": e[c]["spec"], "gates": e[c]["gates"],
                  "S": e[c]["S"], "P": e[c]["P"], "out_p": e[c]["out_p"], "out": e[c]["out"]} for c, l in
                 zip(x, self.graph.get_cells()) for e in self.catalog if e[0]["spec"] == l["spec"]]
 
+    def _get_gates(self, configuration):
+        return sum([c["gates"] for c in configuration])
+
+class ERS(ErrorEvaluator, AMOSA.Problem):
+    def __init__(self, graph, catalog, n_vectors, threshold):
+        ErrorEvaluator.__init__(self, graph, catalog, n_vectors, threshold)
+        self.__args = [[g, s, [0] * self.n_vars] for g, s in zip(self.graphs, list_partitioning(self.samples, cpu_count()))]
+        AMOSA.Problem.__init__(self, self.n_vars, [AMOSA.Type.INTEGER] * self.n_vars, [0] * self.n_vars, self.upper_bound, 2, 1)
+
     def evaluate(self, x, out):
         configuration = self._matter_configuration(x)
         for a in self.__args:
             a[2] = configuration
         with Pool(cpu_count()) as pool:
-            error = pool.starmap(evaluate_vectors, self.__args)
+            error = pool.starmap(evaluate_ers, self.__args)
         rs = sum(error) / self.n_vectors
         f1 = rs + 4.5 / self.n_vectors * (1 + np.sqrt(1 + 4 / 9 * self.n_vectors * rs * (1-rs)))
-        f2 = sum([c["gates"] for c in configuration])
+        f2 = self._get_gates(configuration)
         g1 = f1 - self.threshold
         out["f"] = [f1, f2]
         out["g"] = [g1]
 
-def evaluate_vectors(graph, samples, configuration):
+
+def evaluate_ers(graph, samples, configuration):
     return sum([0 if sample["output"] == graph.evaluate(sample["input"], configuration) else 1 for sample in samples])
+
+class AWCE(ErrorEvaluator, AMOSA.Problem):
+    def __init__(self, graph, catalog, n_vectors, threshold, weights):
+        self.weights = weights
+        ErrorEvaluator.__init__(self, graph, catalog, n_vectors, threshold)
+        self.__args = [[g, s, [0] * self.n_vars, self.weights] for g, s in zip(self.graphs, list_partitioning(self.samples, cpu_count()))]
+        AMOSA.Problem.__init__(self, self.n_vars, [AMOSA.Type.INTEGER] * self.n_vars, [0] * self.n_vars, self.upper_bound, 2, 1)
+
+    def evaluate(self, x, out):
+        configuration = self._matter_configuration(x)
+        for a in self.__args:
+            a[2] = configuration
+        # with Pool(cpu_count()) as pool:
+        #     error = pool.starmap(evaluate_awce, self.__args)
+        error = evaluate_awce(self.graph, self.samples, configuration, self.weights)
+        f1 = np.max(error)
+        f2 = self._get_gates(configuration)
+        g1 = f1 - self.threshold
+        out["f"] = [f1, f2]
+        out["g"] = [g1]
+
+def evaluate_awce(graph, samples, configuration, weights):
+    current_outputs = [ graph.evaluate(sample["input"], configuration) for sample in samples ]
+    return np.max([ sum([weights[o["name"]] if sample["output"][o["name"]] != current[o["name"]] else 0 for o in graph.get_po() ]) for sample, current in zip(samples, current_outputs) ])
--- src/Worker.py
+++ src/Worker.py
@@ -40,8 +40,14 @@ class Worker:
         graph = ALSGraph(design)
         print(f"Performing catalog generation using {cpu_count()} threads. Please wait patiently. This may take time.")
         catalog = ALSCatalog(self.__als_conf.catalog, self.__als_conf.solver).generate_catalog(design, self.__als_conf.timeout)
-        problem = ERS(graph, catalog, self.__error_conf.n_vectors, self.__error_conf.threshold)
         print(f"Performing AMOSA heuristic using {cpu_count()} threads. Please wait patiently. This may take time.")
+        problem = None
+        if self.__error_conf.metric == ErrorConfig.Metric.ERS:
+            problem = ERS(graph, catalog, self.__error_conf.n_vectors, self.__error_conf.threshold)
+        elif self.__error_conf.metric == ErrorConfig.Metric.AWCE:
+            self.__error_conf.weights = self.__parse_weights(graph)
+            problem = AWCE(graph, catalog, self.__error_conf.n_vectors, self.__error_conf.threshold, self.__error_conf.weights)
+
         optimizer = AMOSA(self.__amosa_conf)
         optimizer.minimize(problem)
         print(f"Took {optimizer.duration} sec.")
@@ -58,27 +64,32 @@ class Worker:
         parser = argparse.ArgumentParser()
         parser.add_argument("--config", type=str, help="path of the configuration file", default="config.ini")
         parser.add_argument("--source", type=str, help="specify the input HDL source file")
+        parser.add_argument("--weights", type=str, help="specify weights for AWCE evaluation")
         parser.add_argument("--top", type=str, help="specify the top-module name ")
         parser.add_argument("--output", type=str, help="Output directory. Everything will be placed there.", default="output/")
         args, left = parser.parse_known_args()
         sys.argv = sys.argv[:1] + left
         self.__config_file = args.config
         self.__source_file = args.source
+        self.__weights_file = args.weights
         self.__top_module = args.top
         self.__output_dir = args.output
 
     def __config_parser(self):
         config = configparser.ConfigParser()
         config.read(self.__config_file)
+
         self.__als_conf = ALSConfig(
             config["als"]["luttech"] if "luttech" in config["als"] else "6",
             config["als"]["catalog"] if "catalog" in config["als"] else "lut_catalog.db",
             config["als"]["solver"] if "solver" in config["als"] else "boolector",
             int(config["als"]["timeout"]) if "timeout" in config["als"] else 60000)
+
         self.__error_conf = ErrorConfig(
             config["error"]["metric"] if "metric" in config["error"] else "ers",
             float(config["error"]["threshold"]) if "threshold" in config["error"] else .5,
             int(config["error"]["vectors"] if "vectors" in config["error"] else 1000))
+
         self.__amosa_conf = AMOSAConfig(
             int(config["amosa"]["archive_hard_limit"]) if "archive_hard_limit" in config["amosa"] else 50,
             int(config["amosa"]["archive_soft_limit"]) if "archive_soft_limit" in config["amosa"] else 100,
@@ -88,6 +99,18 @@ class Worker:
             float(config["amosa"]["final_temperature"]) if "final_temperature" in config["amosa"] else 0.0000001,
             float(config["amosa"]["cooling_factor"]) if "cooling_factor" in config["amosa"] else 0.8,
             int(config["amosa"]["annealing_iterations"]) if "annealing_iterations" in config["amosa"] else 100)
+
+    def __parse_weights(self, graph):
+        with open(self.__weights_file, "r") as weights_file:
+            raw_data = weights_file.readlines()
+        raw_data = "".join(raw_data)
+        weights = eval(raw_data)
+        po_names = [o["name"] for o in graph.get_po()]
+        for po in po_names:
+            if po not in weights.keys():
+                graph.plot()
+                raise ValueError(f"{po} not found in weight specification for POs {po_names}")
+        return weights
 
     def __read_source(self, design):
         name, extension = os.path.splitext(self.__source_file)
