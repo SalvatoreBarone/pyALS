6b5286871641ce49413b95dced64e8de294fc097
Salvatore <salvator.barone@gmail.com>
2022-01-15 12:36:45 +0100

    refactor: simplifies error computation for DSE

--- src/Error.py
+++ src/Error.py
@@ -53,103 +53,110 @@ class ErrorEvaluator:
         self.n_vectors = n_vectors
         self.threshold = threshold
         self.samples = []
-        PI = graph.get_pi()
-        if self.n_vectors != 0:
-            for _ in range(self.n_vectors):
-                inputs = { i["name"]: bool(random.getrandbits(1)) for i in PI }
-                self.samples.append({"input": inputs, "output": graph.evaluate(inputs)})
-        else:
-            # exhaustive simulations
-            n_inputs = len(graph.get_pi())
-            self.n_vectors = 2 ** n_inputs
-            permutations = [list(i) for i in itertools.product([False, True], repeat = n_inputs)]
-            for perm in permutations:
-                inputs = { i["name"] : p for i, p in zip(PI, perm) }
-                self.samples.append({"input": inputs, "output": graph.evaluate(inputs)})
-        cells = [{"name": c["name"], "spec": c["spec"]} for c in graph.get_cells()]
-        self.upper_bound = [len(e) - 1 for c in cells for e in catalog if e[0]["spec"] == c["spec"]]
-        configuration = self._matter_configuration([0] * self.n_vars)
-        self.baseline_and_gates = self._get_gates(configuration)
+        self._generate_samples()
+        self.__args = [[g, s, [0] * self.n_vars] for g, s in zip(self.graphs, list_partitioning(self.samples, cpu_count()))]
+        self.upper_bound = self._get_upper_bound()
+        self.baseline_and_gates = self._get_baseline_gates()
         print(f"# vars: {self.n_vars}, ub:{self.upper_bound}")
         print(f"Baseline requirements: {self.baseline_and_gates} AIG-nodes")
 
-    def _matter_configuration(self, x):
-        return [{"name": l["name"], "dist": c, "spec": e[0]["spec"], "axspec": e[c]["spec"], "gates": e[c]["gates"],
-                 "S": e[c]["S"], "P": e[c]["P"], "out_p": e[c]["out_p"], "out": e[c]["out"]} for c, l in
-                zip(x, self.graph.get_cells()) for e in self.catalog if e[0]["spec"] == l["spec"]]
+    def _generate_samples(self):
+        PI = self.graph.get_pi()
+        if self.n_vectors != 0:
+            for _ in range(self.n_vectors):
+                inputs = {i["name"]: bool(random.getrandbits(1)) for i in PI}
+                self.samples.append({"input": inputs, "output": self.graph.evaluate(inputs)})
+        else:
+            self.n_vectors = 2 ** len(PI)
+            permutations = [list(i) for i in itertools.product([False, True], repeat = len(PI))]
+            for perm in permutations:
+                inputs = {i["name"]: p for i, p in zip(PI, perm)}
+                self.samples.append({"input": inputs, "output": self.graph.evaluate(inputs)})
 
-    def _get_gates(self, configuration):
-        return sum([c["gates"] for c in configuration])
+    def _matter_configuration(self, x):
+        return [{"name": l["name"], "dist": c, "spec": e[0]["spec"], "axspec": e[c]["spec"], "gates": e[c]["gates"], "S": e[c]["S"], "P": e[c]["P"], "out_p": e[c]["out_p"], "out": e[c]["out"]} for c, l in zip(x, self.graph.get_cells()) for e in self.catalog if e[0]["spec"] == l["spec"]]
+
+    def _get_upper_bound(self):
+        cells = [{"name": c["name"], "spec": c["spec"]} for c in self.graph.get_cells()]
+        return [len(e) - 1 for c in cells for e in self.catalog if e[0]["spec"] == c["spec"]]
+
+    def _get_outputs(self, configuration):
+        for a in self.__args:
+            a[2] = configuration
+        with Pool(cpu_count()) as pool:
+            outputs = pool.starmap(evaluate_output, self.__args)
+        return [ o for output in outputs for o in output ]
+
+    def _get_baseline_gates(self):
+        return get_gates(self._matter_configuration([0] * self.n_vars))
+
+def evaluate_output(graph, samples, configuration):
+    return [{"e" : s["output"], "a" : graph.evaluate(s["input"], configuration)} for s in samples]
+
+def compute_ep(outputs):
+    ns = len(outputs)
+    rs = sum([0 if o["e"] == o["a"] else 1 for o in outputs]) / ns
+    return rs + 4.5 / ns * (1 + np.sqrt(1 + 4 / 9 * ns * rs * (1 - rs)))
+
+def compute_awce(outputs, weights):
+    return np.max([ sum([weights[po] if o["e"][po] != o["a"][po] else 0 for po in weights.keys() ]) for o in outputs ])
+
+def compute_med(outputs, weights):
+    error_hystogram = { i: 0 for i in range(2**len(weights)) }
+    for o in outputs:
+        error = sum([weights[po] if o["e"][po] != o["a"][po] else 0 for po in weights.keys()])
+        error_hystogram[error] += 1
+    return sum([i[0] * i[1] / (2 ** len(weights)) for i in error_hystogram.items()])
+
+def get_gates(configuration):
+    return sum([c["gates"] for c in configuration])
+
+def get_area(design, cell_area):
+    return sum([cell_area[cell.type.str()[1:]] for module in design.selected_whole_modules_warn() for cell in module.selected_cells()])
+
+def get_power(design, cell_power):
+    return sum([cell_power[cell.type.str()[1:]] for module in design.selected_whole_modules_warn() for cell in module.selected_cells()])
 
 class ErrorProbability(ErrorEvaluator, AMOSA.Problem):
     def __init__(self, graph, catalog, n_vectors, threshold):
         ErrorEvaluator.__init__(self, graph, catalog, n_vectors, threshold)
-        self.__args = [[g, s, [0] * self.n_vars] for g, s in zip(self.graphs, list_partitioning(self.samples, cpu_count()))]
         AMOSA.Problem.__init__(self, self.n_vars, [AMOSA.Type.INTEGER] * self.n_vars, [0] * self.n_vars, self.upper_bound, 2, 1)
 
     def evaluate(self, x, out):
         configuration = self._matter_configuration(x)
-        for a in self.__args:
-            a[2] = configuration
-        with Pool(cpu_count()) as pool:
-            error = pool.starmap(evaluate_eprob, self.__args)
-        rs = sum(error) / self.n_vectors
-        f1 = rs + 4.5 / self.n_vectors * (1 + np.sqrt(1 + 4 / 9 * self.n_vectors * rs * (1-rs)))
-        f2 = self._get_gates(configuration)
+        outputs = self._get_outputs(configuration)
+        f1 = compute_ep(outputs)
+        f2 = get_gates(configuration)
         g1 = f1 - self.threshold
         out["f"] = [f1, f2]
         out["g"] = [g1]
-
-
-def evaluate_eprob(graph, samples, configuration):
-    return sum([0 if sample["output"] == graph.evaluate(sample["input"], configuration) else 1 for sample in samples])
 
 class AWCE(ErrorEvaluator, AMOSA.Problem):
     def __init__(self, graph, catalog, n_vectors, threshold, weights):
         self.weights = weights
         ErrorEvaluator.__init__(self, graph, catalog, n_vectors, threshold)
-        self.__args = [[g, s, [0] * self.n_vars, self.weights] for g, s in zip(self.graphs, list_partitioning(self.samples, cpu_count()))]
         AMOSA.Problem.__init__(self, self.n_vars, [AMOSA.Type.INTEGER] * self.n_vars, [0] * self.n_vars, self.upper_bound, 2, 1)
 
     def evaluate(self, x, out):
         configuration = self._matter_configuration(x)
-        for a in self.__args:
-            a[2] = configuration
-        with Pool(cpu_count()) as pool:
-            error = pool.starmap(evaluate_awce, self.__args)
-        f1 = np.max(error)
-        f2 = self._get_gates(configuration)
+        outputs = self._get_outputs(configuration)
+        f1 = compute_awce(outputs, self.weights)
+        f2 = get_gates(configuration)
         g1 = f1 - self.threshold
         out["f"] = [f1, f2]
         out["g"] = [g1]
-
-def evaluate_awce(graph, samples, configuration, weights):
-    current_outputs = [ graph.evaluate(sample["input"], configuration) for sample in samples ]
-    return np.max([ sum([weights[o] if sample["output"][o] != current[o] else 0 for o in weights.keys() ]) for sample, current in zip(samples, current_outputs) ])
 
 class MED(ErrorEvaluator, AMOSA.Problem):
     def __init__(self, graph, catalog, n_vectors, threshold, weights):
         self.weights = weights
         ErrorEvaluator.__init__(self, graph, catalog, n_vectors, threshold)
-        self.__args = [[g, s, [0] * self.n_vars, self.weights] for g, s in zip(self.graphs, list_partitioning(self.samples, cpu_count()))]
         AMOSA.Problem.__init__(self, self.n_vars, [AMOSA.Type.INTEGER] * self.n_vars, [0] * self.n_vars, self.upper_bound, 2, 1)
 
     def evaluate(self, x, out):
         configuration = self._matter_configuration(x)
-        for a in self.__args:
-            a[2] = configuration
-        with Pool(cpu_count()) as pool:
-            hystogram = pool.starmap(evaluate_med, self.__args)
-        f1 = sum([i[0] * i[1] / (2**len(self.weights)) for i in dict(functools.reduce(operator.add, map(collections.Counter, hystogram))).items()])
-        f2 = self._get_gates(configuration)
+        outputs = self._get_outputs(configuration)
+        f1 = compute_med(outputs, self.weights)
+        f2 = get_gates(configuration)
         g1 = f1 - self.threshold
         out["f"] = [f1, f2]
         out["g"] = [g1]
-
-def evaluate_med(graph, samples, configuration, weights):
-    error_hystogram = { i: 0 for i in range(2**len(weights)) }
-    for sample in samples:
-        current_output = graph.evaluate(sample["input"], configuration)
-        error = sum([weights[o] if sample["output"][o] != current_output[o] else 0 for o in weights.keys()])
-        error_hystogram[error] += 1
-    return error_hystogram
