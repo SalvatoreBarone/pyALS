219f11622097928b3fd6cb1f74c5f75067624961
Salvatore Barone <salvatore.barone@unina.it>
2023-07-15 00:22:08 +0200

    fix: flat files

--- resources/do_synth.tcl
+++ resources/do_synth.tcl
@@ -1,5 +1,7 @@
 puts $hdl_source
 puts $top_module
+puts $flat_netlist
+puts $sdf_file
 set leakage_optimization true
 set dynamic_optimization true
 set compile_clock_gating_through_hierarchy true
@@ -8,6 +10,10 @@ current_design $top_module
 analyze -format verilog $hdl_source
 elaborate $top_module
 compile_ultra
+
+write -hierarchy -format verilog -output $flat_netlist
+write_sdf $sdf_file
+
 report_area > ./area.txt
 report_power > ./power.txt
 quit
