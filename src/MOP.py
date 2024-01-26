fix: bugfix in ep computation

src/MOP.py
@@ -219,7 +219,7 @@ class MOP(pyamosa.Problem):
 
     def get_ep(self, outputs, weights):
         rs = sum(o["e"] != o["a"] for o in outputs) / len(outputs)
-        if self.n_vectors != 0:
+        if self.error_config.n_vectors != 0:
             return float(np.min([1.0, rs + 4.5 / self.n_vectors * (1 + np.sqrt(1 + 4 / 9 * self.n_vectors * rs * (1 - rs)))]))
         else:
             return float(rs)